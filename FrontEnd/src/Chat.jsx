107c825eb9606b1bf9a9cf134f39d28aadc37572
jayarani.ys <jayaraniys1@gmail.com>
2026-02-13 11:12:29 +0530

    front end initialized

---
+++ FrontEnd/src/Chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Chat.css';
+export default function Chat()  {
+    return (
+        <div className="chat">
+            <h1>Chat Component</h1>
+        </div>
+    );
+}
---
+++ FrontEnd/src/ChatWindow.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ChatWindow.css';
+export default function ChatWindow()    {
+    return (
+        <div className="chat-window">
+            <h2>Chat Window</h2>
+        </div>
+    );
+}
